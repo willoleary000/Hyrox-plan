3d5aef38262e835442e41b5ad75efb28f93b5ac8
willoleary000 <willoleary@me.com>
2026-05-07 13:44:02 +0100

    runna.js

--- netlify/functions/runna.js
+++ netlify/functions/runna.js
@@ -1,24 +1,19 @@
 const https = require(“https”);
 
-exports.handler = async function(event, context) {
-const icalUrl = “https://cal.runna.com/20b5653689c52dddbf577b3acf9a80bc.ics”;
-
-const body = await new Promise(function(resolve, reject) {
-https.get(icalUrl, function(res) {
-const chunks = [];
-res.on(“data”, function(chunk) { chunks.push(chunk); });
-res.on(“end”, function() { resolve(Buffer.concat(chunks).toString(“utf8”)); });
-res.on(“error”, reject);
-}).on(“error”, reject);
+exports.handler = async function() {
+const data = await new Promise(function(res, rej) {
+https.get(“https://cal.runna.com/20b5653689c52dddbf577b3acf9a80bc.ics”, function(r) {
+let s = “”;
+r.on(“data”, function(c) { s += c; });
+r.on(“end”, function() { res(s); });
+}).on(“error”, rej);
 });
-
 return {
 statusCode: 200,
 headers: {
-“Content-Type”: “text/calendar; charset=utf-8”,
-“Access-Control-Allow-Origin”: “*”,
-“Cache-Control”: “public, max-age=3600”
+“Content-Type”: “text/calendar”,
+“Access-Control-Allow-Origin”: “*”
 },
-body: body
+body: data
 };
 };
