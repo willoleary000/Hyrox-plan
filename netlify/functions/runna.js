6259413219cd0244e30bcda3293167051e713870
willoleary000 <willoleary@me.com>
2026-05-07 13:41:27 +0100

    runna.js

--- netlify/functions/runna.js
+++ netlify/functions/runna.js
@@ -1,23 +1,24 @@
-const https = require(‘https’);
+const https = require(“https”);
 
-exports.handler = async function() {
-const url = ‘https://cal.runna.com/20b5653689c52dddbf577b3acf9a80bc.ics’;
+exports.handler = async function(event, context) {
+const icalUrl = “https://cal.runna.com/20b5653689c52dddbf577b3acf9a80bc.ics”;
 
-const data = await new Promise(function(resolve, reject) {
-https.get(url, function(res) {
-let body = ‘’;
-res.on(‘data’, function(chunk) { body += chunk; });
-res.on(‘end’, function() { resolve(body); });
-}).on(‘error’, reject);
+const body = await new Promise(function(resolve, reject) {
+https.get(icalUrl, function(res) {
+const chunks = [];
+res.on(“data”, function(chunk) { chunks.push(chunk); });
+res.on(“end”, function() { resolve(Buffer.concat(chunks).toString(“utf8”)); });
+res.on(“error”, reject);
+}).on(“error”, reject);
 });
 
 return {
 statusCode: 200,
 headers: {
-‘Content-Type’: ‘text/calendar; charset=utf-8’,
-‘Access-Control-Allow-Origin’: ‘*’,
-‘Cache-Control’: ‘public, max-age=3600’
+“Content-Type”: “text/calendar; charset=utf-8”,
+“Access-Control-Allow-Origin”: “*”,
+“Cache-Control”: “public, max-age=3600”
 },
-body: data
+body: body
 };
 };
