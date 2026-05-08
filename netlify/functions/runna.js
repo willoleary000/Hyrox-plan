f1d71b73524fa8246a2cff4e329edc950f220dcd
willoleary000 <willoleary@me.com>
2026-05-08 05:03:13 +0100

    Update runna.js

--- netlify/functions/runna.js
+++ netlify/functions/runna.js
@@ -1,20 +1,16 @@
 var https = require(“https”);
 exports.handler = function(event, context, callback) {
 https.get(“https://cal.runna.com/20b5653689c52dddbf577b3acf9a80bc.ics”, function(res) {
-var body = “”;
-res.on(“data”, function(c) { body += c; });
+var data = “”;
+res.on(“data”, function(c) { data += c; });
 res.on(“end”, function() {
 callback(null, {
 statusCode: 200,
-headers: {
-“Content-Type”: “text/calendar”,
-“Access-Control-Allow-Origin”: “*”,
-“Cache-Control”: “no-cache”
-},
-body: body
+headers: { “Content-Type”: “text/calendar”, “Access-Control-Allow-Origin”: “*”, “Cache-Control”: “no-cache” },
+body: data
 });
 });
-}).on(“error”, function(err) {
-callback(null, { statusCode: 500, body: err.message });
+}).on(“error”, function(e) {
+callback(null, { statusCode: 500, body: e.message });
 });
 };
